8c30b8d4a572edfb3b3cf0ad1b4dc1b0c3ca6aae
ShanWan <shan.wan.loo@gmail.com>
2017-03-07 15:31:29 +0800

    fixes the routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require('path')
 const passport = require('./config/ppConfig')
+const session = require('express-session')
 const flash = require('connect-flash')
 const isLoggedIn = require('./middleware/isLoggedIn')
 const userAuth = require('./controllers/auth')
@@ -16,19 +17,15 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chatertain')
 
 mongoose.Promise = global.Promise
 app.set('view engine', 'ejs')
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
-// Log requests to API using morgan
 app.use(morgan('dev'))
-// Enable CORS from client-side
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  next()
-})
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -41,9 +38,9 @@ app.use(function (req, res, next) {
   next()
 })
 
-// app.get('/', function (req, res) {
-//   res.render('profile')
-// })
+app.get('/', function (req, res) {
+  res.render('/auth/login')
+})
 
 app.use('/auth', userAuth)
 
