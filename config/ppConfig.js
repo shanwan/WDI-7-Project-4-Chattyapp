3feee5144e38d66091f40128b209a5303c5f8cf6
ShanWan <shan.wan.loo@gmail.com>
2017-03-07 15:23:52 +0800

    abandons JWT

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -1,10 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const JwtStrategy = require('passport-jwt').Strategy
-const ExtractJwt = require('passport-jwt').ExtractJwt
 const User = require('../models/user')
-// using email instead of username
-// A successful local login will yield the user a JSON Web Token to use to authenticate future requests automatically.
 
 passport.serializeUser(function (user, done) {
   done(null, user.id)
@@ -27,30 +23,5 @@ passport.use(new LocalStrategy({
     return done(null, user)
   })
 }))
-
-//  JWT authentication options
-const jwtOptions = {
-  // Telling Passport to check authorization headers for JWT
-  jwtFromRequest: ExtractJwt.fromAuthHeader(),
-  // Telling Passport where to find the secret - need to put this in .env file
-  secretOrKey: process.env.JWT_SECRET
-}
-
-// set up our JWT login strategy and pass our options through
-passport.use(new JwtStrategy(jwtOptions, function (payload, done) {
-  console.log(payload)
-  User.findById(payload._id, function (err, user) {
-    console.log(payload)
-    if (err) {
-      return done(err, false)
-    }
-    if (user) {
-      done(null, user)
-    } else {
-      done(null, false)
-    }
-  })
-})
-)
 
 module.exports = passport
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const passport = require('../config/ppConfig')
-const jwt = require('jsonwebtoken')
-// The requireAuth middleware we built attaches the user object to req.user.
-// const requireAuth = passport.authenticate('jwt', { session: false })
-// const requireLogin = passport.authenticate('local', { session: false })
 const router = express.Router()
-
-// generate a JSON web token from the user object we pass in. a lot of information to eventually store in a cookie.
-function generateToken (user) {
-  return jwt.sign(user, process.env.JWT_SECRET, {
-    expiresIn: 10080 // in seconds
-  })
-}
 
 router.get('/signup', function (req, res) {
   res.render('auth/signup')
@@ -43,30 +32,14 @@ router.post('/signup', function (req, res) {
   })
 })
 
-// Set user info from request
-function setUserInfo (request) {
-  return {
-    _id: request._id,
-    firstName: request.profile.firstName,
-    lastName: request.profile.lastName,
-    email: request.email,
-    role: request.role
-  }
-}
-
 // to login
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/auth/login',
   failureFlash: 'Invalid username and/or password',
   successFlash: 'You have logged in'
-}), function (req, res, next) {
-  let userInfo = setUserInfo(req.user)
-  res.status(200).json({
-    token: 'JWT ' + generateToken(userInfo),
-    user: userInfo
-  })
 })
+)
 
 router.get('/logout', function (req, res) {
   req.logout()
